01873104461a7eafc428a7963cdce4f421cdd146
Mithin <mithingthomas@gmail.com>
2022-11-28 13:19:21 -0500

    netlify function setup

--- express/lib/mongoDB.js
+++ express/lib/mongoDB.js
@@ -5,18 +5,10 @@ import { MongoClient } from "mongodb";
  * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
  * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
  */
-<<<<<<< HEAD
-let uri;
-if (process.env.NODE_ENV !== "production") {
-  uri = 'mongodb://localhost:27017/wordle-tracker';
-} else {
-  uri = `mongodb+srv://admin:${process.env.PASSWORD}@${process.env.CLUSTER}.mongodb.net/?retryWrites=true&w=majority`
-=======
 let uri = `mongodb+srv://admin:${process.env.PASSWORD}@${process.env.CLUSTER}.mongodb.net/?retryWrites=true&w=majority?maxIdleTimeMS=10000`;
 
 if (process.env.NODE_ENV !== "production") {
   uri = 'mongodb://localhost:27017/wordle-tracker?maxIdleTimeMS=10000';
->>>>>>> 6727667d849b8d8e2815b0da3fa544ac1bc4e965
 }
 
 const client = new MongoClient(uri);
